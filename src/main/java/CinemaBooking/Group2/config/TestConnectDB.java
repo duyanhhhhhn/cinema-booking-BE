a0e5c8ae50214f65d22f2a9a93b3d30c0d4e1146
Codeser <haucoderfullstack05@gmail.com>
2025-12-08 22:10:40 +0700

    update

---
+++ src/main/java/CinemaBooking/Group2/config/TestConnectDB.java
@@ -0,0 +1,29 @@
+package CinemaBooking.Group2.config;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConnectDB {
+    @Bean
+    public CommandLineRunner testDatabaseConnection(DataSource dataSource) {
+        return args -> {
+            System.out.println("=== Đang kiểm tra kết nối MySQL... ===");
+            try (Connection connection = dataSource.getConnection()) {
+                System.out.println("=> Kết nối MySQL THÀNH CÔNG!");
+                System.out.println("=> Catalog: " + connection.getCatalog());
+            } catch (SQLException e) {
+                System.out.println("=> Kết nối MySQL THẤT BẠI!");
+                System.out.println("=> Lý do: " + e.getMessage());
+                e.printStackTrace();
+            }
+            System.out.println("=== Kiểm tra kết nối MySQL xong. ===");
+        };
+    }
+}
