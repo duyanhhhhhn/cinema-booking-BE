3814c141af65fc2002167ede10b6ec5f50e73d7d
Duy Anh <anhduy@dosiinvn.com>
2025-12-10 23:08:22 +0700

    fix restcontroller

--- src/main/java/CinemaBooking/Group2/controllers/client/MovieController.java
+++ src/main/java/CinemaBooking/Group2/controllers/client/MovieController.java
@@ -7,11 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import CinemaBooking.Group2.dtos.movie.MovieResponse;
 import CinemaBooking.Group2.service.MovieService;
 
-@Controller
+@RestController
 @RequestMapping("/api/movie")
 public class MovieController {
 	@Autowired 
