8357d2ba509b2a1f545953d9ea1899119bc2ea5d
ngoclu90 <145348468+ngoclu90@users.noreply.github.com>
2025-12-12 18:51:12 +0700

    gửi otp
    fix truyền dữ liệu chỉ truyền mail

---
+++ src/main/java/CinemaBooking/Group2/dtos/auth/SendOtpRequestDTO.java
@@ -0,0 +1,8 @@
+package CinemaBooking.Group2.dtos.auth;
+
+public class SendOtpRequestDTO {
+	private String email;
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
