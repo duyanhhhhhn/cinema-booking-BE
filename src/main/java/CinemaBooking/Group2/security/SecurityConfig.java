2d84367592898b7ee78924ac997d363688b9a0b6
HauCoder2005 <haucoderfullstack05@gmail.com>
2026-01-18 18:12:55 +0700

    update

--- src/main/java/CinemaBooking/Group2/security/SecurityConfig.java
+++ src/main/java/CinemaBooking/Group2/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package CinemaBooking.Group2.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -71,6 +72,10 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**")
                         .permitAll()
+                        
+                        // ======= PUBLIC =======
+                        .requestMatchers("/api/client/reviews/*/comment").permitAll()
+
 
                         // ===== AUTHENTICATED =====
                         .requestMatchers(
