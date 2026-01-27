dc15289d1a82e1d95314f254060d0f4a3df2eaa1
minhphat1612 <minhphat1612@gmail.com>
2026-01-28 05:31:16 +0700

    change

--- src/main/java/CinemaBooking/Group2/controllers/admin/MarketingAdminController.java
+++ src/main/java/CinemaBooking/Group2/controllers/admin/MarketingAdminController.java
@@ -2,18 +2,25 @@ package CinemaBooking.Group2.controllers.admin;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import CinemaBooking.Group2.dtos.ApiResponse;
 import CinemaBooking.Group2.dtos.concession.VoucherResponseDTO;
 import CinemaBooking.Group2.dtos.marketing.ListPostResponseDTO;
+import CinemaBooking.Group2.dtos.marketing.PostResponseDTO;
+import CinemaBooking.Group2.models.Post;
 import CinemaBooking.Group2.models.Voucher;
 import CinemaBooking.Group2.models.Enum.DiscountType;
 import CinemaBooking.Group2.service.MarketingService;
+import CinemaBooking.Group2.ultis.FileUltility;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,6 +50,38 @@ public class MarketingAdminController {
 		}
 		
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(item); 
+	}
+	@PostMapping("/admin/api/posts")
+	public ResponseEntity<ApiResponse<PostResponseDTO>> addPost(@RequestBody PostResponseDTO dto
+			,@RequestParam("Image")MultipartFile file){
+		try {
+			if(dto==null) {
+				
+			}
+			else{
+				String url = FileUltility.uploadFileImage(file, "/uploads/post", "post");
+				Post item = new Post();
+				item.setCoverUrl(url);
+				item.setTitle(dto.getTitle());
+				item.setCategory(item.getCategory());
+				item.setContent(dto.getContent());
+				item.setSlug(dto.getSlug());
+				item.setExcerpt(dto.getExcerpt());
+				item.setPublished(1);
+				item.setCreatedAt(LocalDateTime.now());
+				PostResponseDTO res=service.newPost(item);
+				if(res.getIsSuccess()) {
+					return ResponseEntity.ok(new ApiResponse<PostResponseDTO>("Created", res));
+				}
+				else {
+				}
+			}
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			System.out.print(e.getMessage());
+		}
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse<PostResponseDTO>("Error", null));
 	}
 	@GetMapping("/admin/api/vouchers")
 	@CrossOrigin
---
+++ src/main/java/CinemaBooking/Group2/dtos/marketing/PostRequestDTO.java
@@ -0,0 +1,65 @@
+package CinemaBooking.Group2.dtos.marketing;
+
+import java.time.LocalDateTime;
+
+public class PostRequestDTO {
+    private String title;
+    private String slug;
+    private String excerpt;
+    private String content;
+    private String category;
+    private String coverUrl;
+    private int published;
+    private LocalDateTime publishedAt;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSlug() {
+		return slug;
+	}
+	public void setSlug(String slug) {
+		this.slug = slug;
+	}
+	public String getExcerpt() {
+		return excerpt;
+	}
+	public void setExcerpt(String excerpt) {
+		this.excerpt = excerpt;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getCoverUrl() {
+		return coverUrl;
+	}
+	public void setCoverUrl(String coverUrl) {
+		this.coverUrl = coverUrl;
+	}
+	public int getPublished() {
+		return published;
+	}
+	public void setPublished(int published) {
+		this.published = published;
+	}
+	public LocalDateTime getPublishedAt() {
+		return publishedAt;
+	}
+	public void setPublishedAt(LocalDateTime publishedAt) {
+		this.publishedAt = publishedAt;
+	}
+	public PostRequestDTO() {
+		
+	}
+}
